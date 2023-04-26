63b5c43c54ca58cb3f455f3bdea8a69c11dceb9b
Derlys <derlysr6@gmail.com>
2023-04-26 05:57:25 -0300

    Exercises

---
+++ src/main/java/org/example/list/Temperatura.java
@@ -0,0 +1,9 @@
+package org.example.list;
+
+public class Temperatura {
+    //faça um programa que calcule a média de temperatura dos 6 primeiros meses do ano e armazene-as em uma
+    //lista de temperaturas. Após isto, calcule a média semestral da temperatura e mostre todas as temperaturas
+    //acima desta média, e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro e etc).
+    //ToDo
+
+}
